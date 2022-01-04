d6d76e737889fc5b511f2ce1d8c70fff6231c1df
edrobertsrayne <ed.rayne@gmail.com>
2022-01-04 16:17:39 +0000

    Dummy blog posts on home page

--- app.py
+++ app.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Ed',
+        'tite': 'Post 1',
+        'content': 'First post',
+        'date_posted': 'January 4th, 2022'
+    },
+    {
+        'author': 'Pearl',
+        'tite': 'Post 2',
+        'content': '2nd post',
+        'date_posted': 'July 3rd, 2021'
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
